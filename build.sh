c9bd7be10970924519f1d5509b3d857fe16b688d
Arnie97 <arnie97@gmail.com>
2017-04-28 07:52:02 +0000

    Use python interpreter in AFDKO instead of the system installation.

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@ family=Hasklig
 romanWeights='Black Bold ExtraLight Light Medium Regular Semibold'
 italicWeights='BlackIt BoldIt ExtraLightIt LightIt MediumIt It SemiboldIt'
 
+# import $AFDKO_Python
+cmdDirPath=$FDK_EXE
+. "${FDK_EXE}/setFDKPaths"
+
 # path to Python script that adds the SVG table
 addSVG=$(cd $(dirname "$0") && pwd -P)/addSVGtable.py
 
@@ -19,12 +23,12 @@ for w in $romanWeights
 do
   makeotf -f Roman/$w/font.ufo -r -ci "$UVS" -o $otfDir/$family-$w.otf
   rm Roman/$w/current.fpr # remove default options file from the source tree after building
-  "$addSVG" $otfDir/$family-$w.otf svg
+  $AFDKO_Python "$addSVG" $otfDir/$family-$w.otf svg
 done
 
 for w in $italicWeights
 do
   makeotf -f Italic/$w/font.ufo -r -ci "$UVS" -o $otfDir/$family-$w.otf
   rm Italic/$w/current.fpr # remove default options file from the source tree after building
-  "$addSVG" $otfDir/$family-$w.otf svg
+  $AFDKO_Python "$addSVG" $otfDir/$family-$w.otf svg
 done
